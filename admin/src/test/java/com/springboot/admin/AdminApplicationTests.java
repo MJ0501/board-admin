8cb2fc63881968e82f53ea775e9e839ce7779940
MJ <mjay.do@gmail.com>
2024-12-29 17:39:15 +0900

    기본골격

---
+++ admin/src/test/java/com/springboot/admin/AdminApplicationTests.java
@@ -0,0 +1,14 @@
+package com.springboot.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
